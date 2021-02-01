cd81bfba85d4576243f682d396c6ed8195b8e88c
gty215615 <1573801768@qq.com>
2021-02-01 15:03:30 +0800

    update template

--- src/index.test.ts
+++ src/index.test.ts
@@ -4,7 +4,7 @@ let logInfo = ''
 const log = console.log = jest.fn((text:string)=> logInfo = text)
 
 describe('it is a test',()=>{
-    it('1 + 2 = 3 ',()=>{
+    it('1 + 2 = 3',()=>{
         expect(sum(1,2)).toBe(3)
         console.log(sum(1,2));  
         expect(logInfo).toBe(3)
